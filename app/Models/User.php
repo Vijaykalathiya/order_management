1df648ce42aa5252f81cbdb247cd8113037d7b5b
Vijay Kalathiya <vijaykalathiya8@gmail.com>
2025-11-23 14:17:40 +0000

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
 
-    use Notifiable;
+    use Notifiable, HasFactory;
 
     protected $fillable = [
         'name',
